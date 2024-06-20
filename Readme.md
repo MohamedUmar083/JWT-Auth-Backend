31f99923003c4eca7135748526e052508c26dd75
MohamedUmar083 <umarmohd777@gmail.com>
2024-06-21 01:15:45 +0530

    Readme Updated and URL changed for Node mailer

--- Readme.md
+++ Readme.md
@@ -3,4 +3,4 @@ INTRODUCTION :
 The Task is to implement a correct password reset flow with email verification, JWT token verification and proper update of the new password in the database for a web app
 
 1. For Frontend Source code Refer [Click Here](https://github.com/MohamedUmar083/JWT-Auth-Frontend)
-2. For Api Documentation [Click Here](https://documenter.getpostman.com/view/25526528/2sA3XQiN3w)
+2. For Api Documentation [Click Here](https://documenter.getpostman.com/view/25526528/2sA3XV8f28)
--- Service/nodemailer.js
+++ Service/nodemailer.js
@@ -21,7 +21,7 @@ const sendPasswordResetEmail = async (userMail, userID) => {
       html: `Dear ${userMail},
 We have received a request to reset the password for your account associated with the ID ${userID}.
 If you did not request a password reset, please ignore this email. Your password will remain unchanged.
-To reset your password, please click the link to redirect : <a href=http://localhost:3000/resetpassword/${userID}>${shorturl}</>`, // html body
+To reset your password, please click the link to redirect : <a href=https://jwt-auth-frnt.netlify.app/resetpassword/${userID}>${shorturl}</>`, // html body
     });
     return { success: true, messageId: info.messageId };
   } catch (error) {
